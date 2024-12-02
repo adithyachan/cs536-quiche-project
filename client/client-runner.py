ad362800b5a2536a5b8d59c15a4b8bd9987c2545
Matt Hiatt <mwhiatt@purdue.edu>
2024-12-02 02:59:41 +0000

    fix: client-runner var naming bug

--- client/client-runner.py
+++ client/client-runner.py
@@ -2,22 +2,22 @@ import subprocess as sp
 import time
 import pandas as pd
 
-tcp_client = "curl"
-quiche_client = "./client/quiche-client"
+tcp_client_path = "curl"
+quiche_client_path = "./client/quiche-client"
 server_url = "172.17.0.2"
 server_port = "4433"
 results_dir = "results/"
 
 def quiche_client(path="/"):
     start_time = time.time()
-    sp.run([quiche_client, "https://" + server_url + ":" + server_port + path, "--no-verify"], stdout = sp.DEVNULL, stderr = sp.DEVNULL) 
+    sp.run([quiche_client_path, "https://" + server_url + ":" + server_port + path, "--no-verify"], stdout = sp.DEVNULL, stderr = sp.DEVNULL) 
     end_time = time.time()
     print(path, "(QUICHE):", end_time - start_time)
     return end_time - start_time
 
 def http_client(path="/"):
     start_time = time.time()
-    sp.run([tcp_client, "-k", "https://" + server_url + ":" + server_port + path, "-o", "/dev/null"], stdout = sp.DEVNULL, stderr = sp.DEVNULL)
+    sp.run([tcp_client_path, "-k", "https://" + server_url + ":" + server_port + path, "-o", "/dev/null"], stdout = sp.DEVNULL, stderr = sp.DEVNULL)
     end_time = time.time()
     print(path, "(TCP):", end_time - start_time)
     return end_time - start_time
@@ -36,4 +36,5 @@ def measure_download_speeds(client=quiche_client, output_file="out.csv"):
 
 
 if __name__ == "__main__":
+    measure_download_speeds(client=quiche_client, output_file="10mbps_Q.csv")
     measure_download_speeds(client=http_client, output_file="10mbps_HTTP.csv")
